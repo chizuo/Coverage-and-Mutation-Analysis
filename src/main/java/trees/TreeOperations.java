14da40733b971f8df65f6199ad5a7e7b78bd06ca
Jonathan <jon.chua51@gmail.com>
2022-10-10 14:16:20 -0700

    Added boilerplate for simplicity

--- src/main/java/trees/TreeOperations.java
+++ src/main/java/trees/TreeOperations.java
@@ -1,5 +1,17 @@
 package trees;
 
+import java.util.ArrayList;
+
 public class TreeOperations {
-    // TODO: write any needed code here
+    public static <A> ArrayList<A> bfs(final Node<A> root) {
+        return new ArrayList<A>();
+    }
+
+    public static <A> ArrayList<A> preorder(final Node<A> root) {
+        return new ArrayList<A>();
+    }
+
+    public static <A> int maxDepth(final Node<A> root) {
+        return 0;
+    }
 }
