91fea3168748668683689aa2b2145c2862e78f76
Jonathan <jon.chua51@gmail.com>
2022-10-10 14:33:27 -0700

    initial commit for preorder

--- src/main/java/trees/TreeOperations.java
+++ src/main/java/trees/TreeOperations.java
@@ -1,6 +1,8 @@
 package trees;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TreeOperations {
     public static <A> ArrayList<A> bfs(final Node<A> root) {
@@ -8,7 +10,10 @@ public class TreeOperations {
     }
 
     public static <A> ArrayList<A> preorder(final Node<A> root) {
-        return new ArrayList<A>();
+        ArrayList<A> preOrder = new ArrayList<A>();
+        Queue<Node<A>> queue = new LinkedList<Node<A>>();
+        queue.add(root);
+        return preOrder;
     }
 
     public static <A> int maxDepth(final Node<A> root) {
