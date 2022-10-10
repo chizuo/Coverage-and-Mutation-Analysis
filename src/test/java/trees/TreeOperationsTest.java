28db47b5c00646280cdeb360ca74e4d3e69aabe7
Jonathan <jon.chua51@gmail.com>
2022-10-10 15:57:37 -0700

    updated unit test so tree is usable by all

--- src/test/java/trees/TreeOperationsTest.java
+++ src/test/java/trees/TreeOperationsTest.java
@@ -3,21 +3,22 @@ package trees;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
 
 public class TreeOperationsTest {
-    @Test
+    Node<String> H = new Node<String>("H", null, null);
+    Node<String> I = new Node<String>("I", H, null);
+    Node<String> C = new Node<String>("C", null, null);
+    Node<String> E = new Node<String>("E", null, null);
+    Node<String> A = new Node<String>("A", null, null);
+    Node<String> D = new Node<String>("D", C, E);
+    Node<String> B = new Node<String>("B", A, D);
+    Node<String> G = new Node<String>("G", null, I);
+    Node<String> F = new Node<String>("F", B, G);
+    ArrayList<String> expects = new ArrayList<String>();
 
-    public void Testpreorder() {
-        Node<String> H = new Node<String>("H", null, null);
-        Node<String> I = new Node<String>("I", H, null);
-        Node<String> C = new Node<String>("C", null, null);
-        Node<String> E = new Node<String>("E", null, null);
-        Node<String> A = new Node<String>("A", null, null);
-        Node<String> D = new Node<String>("D", C, E);
-        Node<String> B = new Node<String>("B", A, D);
-        Node<String> G = new Node<String>("G", null, I);
-        Node<String> F = new Node<String>("F", B, G);
-        ArrayList<String> expects = new ArrayList<String>();
+    @Before
+    void buildTree() {
         expects.add("F");
         expects.add("B");
         expects.add("A");
@@ -27,6 +28,10 @@ public class TreeOperationsTest {
         expects.add("G");
         expects.add("I");
         expects.add("H");
+    }
+
+    @Test
+    public void Testpreorder() {
         assertEquals(expects, TreeOperations.preorder(F));
         assertEquals(TreeOperations.preorder(F), TreeOperations.preorder(F));
     }
