0f751c3a65883861a11b30a4c70a5274fc9628de
Jonathan <jon.chua51@gmail.com>
2022-10-10 15:28:04 -0700

    Implemented preorder method and unit test testpreorder

--- src/main/java/trees/TreeOperations.java
+++ src/main/java/trees/TreeOperations.java
@@ -3,16 +3,27 @@ package trees;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class TreeOperations {
     public static <A> ArrayList<A> bfs(final Node<A> root) {
-        return new ArrayList<A>();
+        Queue<Node<A>> queue = new LinkedList<Node<A>>();
+        var results = new ArrayList<A>();
+        return results;
     }
 
     public static <A> ArrayList<A> preorder(final Node<A> root) {
         ArrayList<A> preOrder = new ArrayList<A>();
-        Queue<Node<A>> queue = new LinkedList<Node<A>>();
-        queue.add(root);
+        Stack<Node<A>> stack = new Stack<Node<A>>();
+        stack.push(root);
+        while (!stack.empty()) {
+            var node = stack.pop();
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+            preOrder.add(node.contents);
+        }
         return preOrder;
     }
 
