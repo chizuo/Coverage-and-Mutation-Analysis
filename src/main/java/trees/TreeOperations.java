681cda9ba7beea7fcc2b7b455961e000789a50e9
Jonathan <jon.chua51@gmail.com>
2022-10-10 22:00:47 -0700

    simplified maxDepth further to blunt mutation areas

--- src/main/java/trees/TreeOperations.java
+++ src/main/java/trees/TreeOperations.java
@@ -39,8 +39,6 @@ public class TreeOperations {
     public static <A> int maxDepth(final Node<A> root) {
         if (root == null)
             return 0;
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        return Math.max(leftDepth, rightDepth) + 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
