685d89bd3cb9cc9cf770d3f8ed6b366b8b10e120
Jonathan <jon.chua51@gmail.com>
2022-10-10 19:38:27 -0700

    merge fixed unit tests

--- src/test/java/trees/TreeOperationsTest.java
+++ src/test/java/trees/TreeOperationsTest.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TreeOperationsTest {
+    Node<String> K = new Node<String>("K", null, null);
+    Node<String> J = new Node<String>("J", null, K);
     Node<String> H = new Node<String>("H", null, null);
-    Node<String> I = new Node<String>("I", H, null);
+    Node<String> I = new Node<String>("I", H, J);
     Node<String> C = new Node<String>("C", null, null);
     Node<String> E = new Node<String>("E", null, null);
     Node<String> A = new Node<String>("A", null, null);
@@ -17,7 +19,7 @@ public class TreeOperationsTest {
     Node<String> F = new Node<String>("F", B, G);
 
     @Test
-    public void Testpreorder() {
+    public void TestPreOrder() {
         ArrayList<String> expects = new ArrayList<String>();
         expects.add("F");
         expects.add("B");
@@ -28,7 +30,18 @@ public class TreeOperationsTest {
         expects.add("G");
         expects.add("I");
         expects.add("H");
+        expects.add("J");
+        expects.add("K");
         assertEquals(expects, TreeOperations.preorder(F));
         assertEquals(TreeOperations.preorder(F), TreeOperations.preorder(F));
+    }
+
+    @Test
+    public void TestMaxDepth() {
+        assertEquals(5, TreeOperations.maxDepth(F));
+        assertEquals(3, TreeOperations.maxDepth(B));
+        assertEquals(4, TreeOperations.maxDepth(G));
+        assertEquals(2, TreeOperations.maxDepth(D));
+        assertEquals(TreeOperations.maxDepth(B), TreeOperations.maxDepth(I));
     }
 }
